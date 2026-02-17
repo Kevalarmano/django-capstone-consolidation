f41d452290b16fa96918df4879cc22df533b126a
Kevalarmano <keval4ram@gmail.com>
2026-02-17 08:58:08 +0000

    Fix Sphinx config and include built HTML docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,27 @@
-project = 'Django Capstone'
-author = 'Keval'
-release = '1.0'
+import os
+import sys
 
+# Add project root to Python path
+sys.path.insert(0, os.path.abspath(".."))
+
+# -- Project information -----------------------------------------------------
+project = "Django Capstone"
+author = "Keval"
+release = "1.0"
+
+# -- General configuration ---------------------------------------------------
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_theme = 'alabaster'
+# -- Options for HTML output -------------------------------------------------
+html_theme = "alabaster"
+
+# --- Django setup for autodoc (IMPORTANT) ---
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "capstone_project.settings")
+import django
+django.setup()
